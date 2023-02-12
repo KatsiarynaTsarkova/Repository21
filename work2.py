24fe99c057ba5d618f85edd47d38900801d3a906
Kate <katya_tsarkov@mail.ru>
2023-02-12 19:00:04 +0300

    work2.py

---
+++ work2.py
@@ -0,0 +1,26 @@
+import telebot
+
+
+#Задача 2. Напишите программу, которая позволяет считывать из файла вопрос, отвечать на него
+# и отправлять ответ обратно пользователю.
+
+
+bot = telebot.TeleBot("5881190851:AAFtLLkvjldXAF7xUbPArEOE1gxqmqXhngo", parse_mode=None)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Добрый день, чем могу помочь?")    
+    
+@bot.message_handler(content_types=['text'])
+def read_text_command(message):
+        data =  open('messages.txt', 'r', encoding='utf-8')
+        r_list = data.readlines()
+        data.close()
+        for id in r_list:
+            bot.send_message(id[:-1], 'Переустановите систему!')#убираем последний символ \n
+           
+     
+      
+ 
+bot.infinity_polling()
